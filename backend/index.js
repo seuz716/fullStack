34caffb6f47450800e97034a2db61e8012f9dae5
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2022-03-29 13:45:03 -0500

    Feature():  FullStack #3 , Express Middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path')
 
 //inicializations
-const app  = express;
+const app  = express();
 
 // Settings
 app.set('port',3000);
 
+//Middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join (__dirname,'public/uploads'),
+    filename(req, file, cb){
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+})
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+
 //start the server
-app.listen(app.get('port'),() => {
-    console.log('Server on port', app.get('port'));
-});
+var server = app.listen(app.get('port'),function(){
+    console.log('express server listening on port ' + server.address().port);
+     })
