02281884a6a8d440df237142aa4c191a1469c34d
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2022-04-01 11:49:44 -0500

    FullStack #7 , REST API Nodejs & Mongodb

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,23 @@
 const {Router}= require('express');
 const router = Router();
 
-router.get('/', (req, res) => res.send('hello world'));
+const Book = require('../models/Book');
 
+router.get('/', async (req, res) => {
+    const books = await Book.find();
+    res.json(books);
+});
+
+router.post('/', async (req, res) => {
+    const {title, author, isbn}= req.body;
+    const newBook = new Book({title,author, isbn});
+    await newBook.save();
+    res.json({message:'Book saved'});
+});
+
+router.delete('/:id', async (req, res) => {
+    const book =await Book.findByIdAndDelete(req.params.id);
+    console.log(book);
+    res.json({message:'Book deleted'});
+});
 module.exports = router;
